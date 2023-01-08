50d02d7a145c2bbcefebcdbac9811cd8e743dea1
bfitz83 <115970022+bfitz83@users.noreply.github.com>
2023-01-08 03:55:54 -0500

    added filter neg numbers
    added filter neg numbers

--- The first 10/Filter negetive numbers/index.js
+++ The first 10/Filter negetive numbers/index.js
@@ -1,24 +1,56 @@
-// this one gets rid of negative numbers, spaces in strings, and counts vowels
+// Elements from HTML
+
+    const numberInput = document.getElementById("number-input")
+    const outputEl = document.getElementById("output-el")
+    const arrayList = document.getElementById("array-list")
+    
+// Buttons
+    
+    const run = document.getElementById("run")
+    const addNumber = document.getElementById("add-number")
+
+// Variables
+
+    let numberArray = []
+    let positiveArray = []
+
+// Event listeners
+
+    addNumber.addEventListener("click", function(){
+        numberArray.push(numberInput.value)
+        arrayList.innerHTML = `${numberArray}`
+        filterNegative()
+    })
+    
+    run.addEventListener("click", function(){
+        console.log(positiveArray)
+        printsPositiveNumbers()
+        
+    })
+    
+    
+// Functions
+    
+    
+    function filterNegative(){
+        if (numberInput.value >= 0){
+            positiveArray.push(numberInput.value)
+        }
+        numberInput.value = ""
+        
+        
+    }
+
+    function printsPositiveNumbers(){
+        outputEl.innerHTML = `${positiveArray}`
+    }
+    
+    
 
 
 
 
-let numberArray = [1, 3, -4, 6, -7]
-console.log(numberArray);
 
-console.log(numberArray.filter(filterNegative));
-
-function filterNegative(x){
-    return x >= 0
-}
-
-
-
-
-
-
-
-filterNegative()
 
 
 
